972d28e47990bb95deb9b08b3f11047938c671b8
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-08 15:42:24 +0200

    retouch panel data

--- src/data/panelData.js
+++ src/data/panelData.js
@@ -1,22 +1,18 @@
  const panelData = [
   {
-    title: "First Panel",
+    title: "Vigilante",
     content: "No, no, no. A vigilante is just a man lost in scramble for his own gratification. He can be destroyed or locked up. But if you make yourself more than just a man, if you devote yourself to an idel and if they can't stop you then you become something else entirely. Legend, Mr Wayne.",
   },
   {
-    title: "Second Panel",
+    title: "The Mountain",
     content: "Tomorrow, you will be released. If you are bored of brawling with thieves and want to achieve something there is a rare blue flower that grows on the eastern slopes. Pick one of these flowers. If you can carry it to the top of the mountain, you may find what you were looking for in the first place.",
   },
   {
-    title: "Third Panel",
+    title: "A World without Batman",
     content: "I had a vision of a world without Batman. The Mob ground out a little profit and the police tried to shut them down one block at a time. And it was so boring. I've had a change of heart. I don't want Mr Reese spoiling everything but why should I have all the fun? Let's give someone else a chance. If Coleman Reese isn't dead in 60 minutes then I blow up a hospital.",
   },
   {
-    title: "Fourth Panel",
-    content: "I'm here to ensure the League of Shadow fulfills its duty to restore balance to civilization. You yourself fought the decadence of Gotham for years with all your strength, all your resources, all your moral authority. And the only victory you achieved was a lie. Now, you understand? Gotham is beyond saving and must be allowed to die.",
-  },
-  {
-    title: "Fifth Panel",
+    title: "Balance",
     content: "I'm here to ensure the League of Shadow fulfills its duty to restore balance to civilization. You yourself fought the decadence of Gotham for years with all your strength, all your resources, all your moral authority. And the only victory you achieved was a lie. Now, you understand? Gotham is beyond saving and must be allowed to die.",
   },
 ]
