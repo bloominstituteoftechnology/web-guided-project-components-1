44c02913dd9329d67264aa9c7a5f603f3bc2bc04
Timothy Roy <timothy.roy@protonmail.ch>
2021-07-12 22:04:40 -0500

    Instantiated elements inside makePanel, added structure, added CSS classes

--- src/index.js
+++ src/index.js
@@ -43,9 +43,9 @@ imageData.forEach(imageDataObject => {
 //  On the other hand, the default export from data/constants.js
 //  Destructure `open` and `close` from the constants
 import panelData from './data/panelData'
-import myArbitraryName from './data/constants'
+import arrows from './data/constants'
 
-const { open, close } = myArbitraryName
+const { open, close } = arrows
 
 // TASK 2- Verify our imports using log statements
 console.log(panelData) // log the panelData
@@ -58,19 +58,20 @@ console.log(close) // log the close arrow
 //  so we can append the code-generated panel to the DOM.
 const accordion = document.querySelector('.accordion'); 
 
+// this function "closes over" the arrows
+
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
 function makePanel({ title, content }) {
   // const { title, content } = panelDataObject
 
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
-
+  const panel = document.createElement('div')
+  const panelBar = document.createElement('div')
+  const panelContent = document.createElement('div')
+  const panelTitle = document.createElement('h3')
+  const panelButtons = document.createElement('div')
+  const openButton = document.createElement('button')
+  const closeButton = document.createElement('button')
 
   // TASK 6- Setup the structure of our elements
   /*
@@ -86,10 +87,23 @@ function makePanel({ title, content }) {
     </div>
   */
 
+    panel.appendChild(panelBar)
+    panel.appendChild(panelContent)
+    panelBar.appendChild(panelTitle);
+    panelBar.appendChild(panelButtons);
+    panelButtons.appendChild(openButton);
+    panelButtons.appendChild(closeButton);
+
 
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
 
+  panel.classList.add('panel')
+  panelBar.classList.add('panel-bar')
+  panelButtons.classList.add("panel-buttons");
+  openButton.classList.add('panel-btn-open');
+  closeButton.classList.add('panel-btn-close', 'hide-btn');
+  panelContent.classList.add('panel-content');
 
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
@@ -105,6 +119,14 @@ function makePanel({ title, content }) {
   return null
 }
 
+makePanel({ title: "Black Widow", content: "Natasha scarjo" })
+
+panelData.forEach(panelDataObj => {
+  // 1. use our makePanel component to create a panel
+
+  // 2. Attach our new UI component to the DOM
+
+})
 
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
